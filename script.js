f1fb08eb1ec424e0ce10c02f8191ae89b8022f3d
beyondmayOwO <78870914+beyondmayOwO@users.noreply.github.com>
2023-11-26 21:17:54 +0630

    Finished the basic calculation

---
+++ script.js
@@ -0,0 +1,43 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a ,b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function remainder(a, b) {
+  return a  % b;
+}
+
+function operate(operator, a, b) {
+  switch (operator) {
+
+    case '+':
+      return add(a, b);
+
+    case '-':
+      return subtract(a, b);
+
+    case '×':
+      return multiply(a ,b);
+
+    case '÷':
+      return divide(a, b);
+
+    case '%':
+      return remainder(a, b);
+
+    default:
+      return null;
+  }
+}
+
