e318e74a4d47f40280e7cfbeb1b48b97241e46cf
beyondmayOwO <78870914+beyondmayOwO@users.noreply.github.com>
2023-11-28 21:20:47 +0630

    Entering the double digits and all clear functions

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function divide(a, b) {
 }
 
 function remainder(a, b) {
-  return a  % b;
+  return a % b;
 }
 
 function operate(operator, a, b) {
@@ -44,11 +44,14 @@ function operate(operator, a, b) {
 //Organizing the screen display section
 const digitBtn = document.querySelectorAll('.digit');
 const operatorBtn = document.querySelectorAll('.operator');
+const equals = document.querySelector('.equals');
+const clear = document.querySelector('.clear');
+const allClear = document.querySelector('.all-clear');
 
 const screen = document.querySelector('.screen');
 const calculation = document.createElement('div');
 const result = document.createElement('div');
-calculation.textContent = '0 + 0';
+calculation.textContent = 0;
 result.textContent = 0;
 
 screen.appendChild(calculation);
@@ -58,36 +61,64 @@ calculation.className = 'calculation';
 result.className = 'result';
 
 let num = 0;
-let operator = '+';
+let operator = '';
 let subsequentNum = 0;
+let answer = 0;
+// Flag to determine whether num or subsequentNum is being entered
+let isTypingNum = true;
 
 digitBtn.forEach(Btn => {
   Btn.addEventListener('click', () => {
-    num = Btn.textContent;
-    updateNum();
-    subsequentNum = Btn.textContent;
-    updateSubsequentNum();
-    //console.log(num);
-    //console.log(subsequentNum);
+    if(isTypingNum) {
+      // To enter double digits. Then converts to number from string using unary plus operator (+)
+      num = +(num + Btn.textContent);
+      updateNum();
+    }
+    else {
+      subsequentNum = +(subsequentNum + Btn.textContent);
+      updateSubsequentNum();
+    }
   })
 })
 
 operatorBtn.forEach(Btn => {
   Btn.addEventListener('click', () => {
     operator = Btn.textContent;
-    //console.log(operator);
+    isTypingNum = false;
     updateOperator();
   })
 })
 
+equals.addEventListener('click', () => {
+  answer = operate(operator, num, subsequentNum);
+  updateAnswer();
+})
+
+allClear.addEventListener('click', reset);
+
+
 function updateNum() {
-  return calculation.textContent = `${num} + 0`;
+  return calculation.textContent = `${num}`;
 }
 
 function updateOperator() {
-  return calculation.textContent = `${num} ${operator} 0`;
+  return calculation.textContent = `${num} ${operator}`;
 }
 
 function updateSubsequentNum() {
   return calculation.textContent = `${num} ${operator} ${subsequentNum}`;
+}
+
+function updateAnswer() {
+  return result.textContent = `${answer}`;
+}
+
+function reset() {
+  num = 0;
+  operator = '';
+  subsequentNum = 0;
+  answer = 0;
+  calculation.textContent = 0;
+  result.textContent = 0;
+  isTypingNum = true;
 }
